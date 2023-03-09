a325039ab1e06236a71aadf779355c7d7d136a82
Tamara <novickaya.toma@mail.ru>
2023-03-09 20:55:49 +0300

    Example006_Function

--- Example003_SUM/Program.cs
+++ Example003_SUM/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int naumberB = 5;
-int result = numberA+naumberB;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA+numberB;
 Console.WriteLine(result);
